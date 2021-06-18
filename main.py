fc200269928a6586220c1f40a523126159e9dc24
nuruszama <85991146+nuruszama@users.noreply.github.com>
2021-06-18 10:52:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -39,6 +39,49 @@ def help(update, context):
     """Send a message when the command /help is issued."""
     update.message.reply_text('Help! I am happy to assist you. Right now, I am still learning')
 
+def alarm(context: CallbackContext) -> None:
+    """Send the alarm message."""
+    context.bot.send_message(chat_id=context.job.context, 
+                             text='beep!')
+
+def remove_job_if_exists(name: str, context: CallbackContext) -> bool:
+    """Remove job with given name. Returns whether job was removed."""
+    current_jobs = context.job_queue.get_jobs_by_name(name)
+    if not current_jobs:
+        return False
+    for job in current_jobs:
+        job.schedule_removal()
+    return True
+
+def set_timer(update: Update, context: CallbackContext) -> None:
+    """Add a job to the queue."""
+    chat_id = update.message.chat_id
+    try:
+        # args[0] should contain the time for the timer in seconds
+        due = int(context.args[0])
+        if due < 0:
+            update.message.reply_text('Sorry, we can not go back to past!')
+            return
+
+        job_removed = remove_job_if_exists(str(chat_id), context)
+        context.job_queue.run_once(alarm, due, context=chat_id, name=str(chat_id))
+
+        text = 'Timer successfully set!'
+        if job_removed:
+            text += ' Old one was removed.'
+        update.message.reply_text(text)
+
+    except (IndexError, ValueError):
+        update.message.reply_text('Usage: /set <seconds>')
+
+
+def unset(update: Update, context: CallbackContext) -> None:
+    """Remove the job if the user changed their mind."""
+    chat_id = update.message.chat_id
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    text = 'Timer successfully cancelled!' if job_removed else 'You have no active timer.'
+    update.message.reply_text(text)
+
 def end(update, context):
     update.message.reply_text(
       'Happy that you send me a message 😀 I cannot completely text you back. I am still learning')
@@ -70,6 +113,8 @@ def main():
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("set", set_timer))
+    dp.add_handler(CommandHandler("unset", unset))
     """when /help or /start is tiggered, bot replies with what we defined with start or help respectively"""
 
     # on noncommand i.e message - welcome message or starting of coversation
