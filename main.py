30a33636cdba9cd43b591e4871ee69efb386f5f9
Abdul Samad K A <85991146+nuruszama@users.noreply.github.com>
2021-06-21 15:05:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
-"""
-Simple Conversation Telegram bot taken from the python-telegram-bot examples.
-"""
+"""Simple bot that can foward your messages to groups/channels and also replies to your message"""
 
 import logging
-from typing import Dict
-
-from telegram import ReplyKeyboardMarkup, Update, ReplyKeyboardRemove
+from telegram import Update
 from telegram.ext import (
-    Updater,
-    CommandHandler,
-    MessageHandler,
-    Filters,
-    ConversationHandler,
-    CallbackContext,
-)
-
+    Updater, CommandHandler, ConversationHandler, MessageHandler,
+    Filters, CallbackContext
+    )
+    
 import os
 PORT = int(os.environ.get('PORT', 5000))
 
@@ -27,172 +19,106 @@ logger = logging.getLogger(__name__)
 """replace token with the token you got from bot father corresponding to your bot"""
 TOKEN = 'token'
 
-CHOOSING, TYPING_CHOICE, TYPING_REPLY = range (3)
-
-reply_keyboard = [
-    ['Age', 'Favourite colour'],
-    ['Number of siblings', 'Something else...'],
-    ['Done'],
-]
-markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
-
-def facts_to_str(user_data: Dict[str, str]) -> str:
-    """Helper function for formatting the gathered user info."""
-    facts = [f'{key} - {value}' for key, value in user_data.items()]
-    return "\n".join(facts).join(['\n', '\n'])
+TYPING_MESSAGE, TYPING_DESTINATION, GO = range(3)
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
 
-def start(update, context):
-    """Send a message when the command /start is issued."""
+#Send a message when the command /forward is issued.
+def forward(update, context) -> None:
     update.message.reply_text(
-        "Hi @#{message.from.username}! \n"
-        "About what you would like to talk to me \n \n"
-        "eg: Name, age, hobbies. Reply anything like this \n"
+        "Give me the message you want to forward \n"
+        "\n"
+        "Type <Exit> to destroy forward command \n"
     )
 
-    return CHOOSING
+    return TYPING_MESSAGE
 
-def regular_choice(update: Update, context: CallbackContext) -> int:
-    """Ask the user for info about the selected predefined choice."""
-    text = update.message.text
-    context.user_data['choice'] = text
-    update.message.reply_text(f'Your {text.lower()}? Yes, I would love to hear about that!')
-
-    return TYPING_REPLY
-
-def custom_choice(update: Update, context: CallbackContext) -> int:
-    """Ask the user for a description of a custom category."""
-    update.message.reply_text(
-        "Alright, please send me the category about which you wish to talk"
-        "for example 'Most impressive skill'"
-        )
-
-    return TYPING_CHOICE
-
-def received_information(update: Update, context: CallbackContext) -> int:
-    """Store info provided by user and ask for the next category."""
-    user_data = context.user_data
-    text = update.message.text
-    category = user_data['choice']
-    user_data[category] = text
-    del user_data['choice']
-
-    update.message.reply_text(
-        "Neat! Just so you know, this is what you already told me:"
-        f"{facts_to_str(user_data)} You can tell me more, or change your opinion"
-        " on something.",
-        reply_markup=markup,
+#taking message to be forwarded as input
+def fwd_msg(update, context) -> None:
+    content = 'update.message.text'
+    
+    context.bot.send_message(chat_id=update.message.chat_id, text=
+        "Neat! Now tell me where to send this message \n"
+        "\n"
+        "Reply with chat id"
     )
 
-    return CHOOSING
+    return TYPING_DESTINATION
 
-def done(update: Update, context: CallbackContext) -> int:
-    """Display the gathered info and end the conversation."""
-    user_data = context.user_data
-    if 'choice' in user_data:
-        del user_data['choice']
-
-    update.message.reply_text(
-        f"I learned these facts about you: {facts_to_str(user_data)}Until next time!",
-        reply_markup=ReplyKeyboardRemove(),
+#taking destination to which message is to be forwarded as input
+def destination(update, context) -> None:
+    destination = 'update.message.text'
+    context.bot.send_message(chat_id=update.message.chat_id, text=
+        "I have recorded your message and chat id.\n\n"
+        "Forwarding your message.....\n"
+        "Done. Check forward destination"
     )
 
-    user_data.clear()
+    return GO
+
+#Sending the message to destination
+def send(update, context) -> None:
+    context.bot.send_message(chat_id=destination, text=content)
+
+def exit(update, context) -> None:
+    context.bot.send_message(chat_id=update.message.chat_id, text=
+        "Destroyed entry to forward command\nuse /forward any time to use it again",
+    )
+
     return ConversationHandler.END
 
-def alarm(context: CallbackContext) -> None:
-    """Send the alarm message."""
-    context.bot.send_message(chat_id=context.job.context, 
-                             text='beep! Here is your reminder')
-
-def remove_job_if_exists(name: str, context: CallbackContext) -> bool:
-    """Remove job with given name. Returns whether job was removed."""
-    current_jobs = context.job_queue.get_jobs_by_name(name)
-    if not current_jobs:
-        return False
-    for job in current_jobs:
-        job.schedule_removal()
-    return True
-
-def set_timer(update: Update, context: CallbackContext) -> None:
-    """Add a job to the queue."""
-    chat_id = update.message.chat_id
-    try:
-        # args[0] should contain the time for the timer in seconds
-        due = int(context.args[0])
-        if due < 0:
-            update.message.reply_text('Sorry, we can not go back to past!')
-            return
-
-        job_removed = remove_job_if_exists(str(chat_id), context)
-        context.job_queue.run_once(alarm, due, context=chat_id, name=str(chat_id))
-
-        text = 'Timer successfully set!'
-        if job_removed:
-            text += ' Old one was removed.'
-        update.message.reply_text(text)
-
-    except (IndexError, ValueError):
-        update.message.reply_text('Usage: /set <seconds>')
-
-def unset(update: Update, context: CallbackContext) -> None:
-    """Remove the job if the user changed their mind."""
-    chat_id = update.message.chat_id
-    job_removed = remove_job_if_exists(str(chat_id), context)
-    text = 'Timer successfully cancelled!' if job_removed else 'You have no active timer.'
-    update.message.reply_text(text)
-
+#Log Errors caused by Updates.
 def error(update, context):
-    """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
+#Start the bot.
 def main():
-    """Start the bot."""
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
     updater = Updater(TOKEN, use_context=True)
-
+    
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
-    # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("help", help))
-    dp.add_handler(CommandHandler("set", set_timer))
-    dp.add_handler(CommandHandler("unset", unset))
-    """when /help or /start is tiggered, bot replies with what we defined with start or help respectively"""
-
-    # on noncommand i.e message - welcome message or starting of coversation
-    conv_handler = ConversationHandler(entry_points, fallbacks)
-        entry_points=[CommandHandler(('start'), start)],
+    # procedure to forward messsage
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('forward', forward)],
         states={
-           CHOOSING: [
-               MessageHandler(Filters.regex('^(Age|Favourite colour|Number of siblings)$'), regular_choice),
-               MessageHandler(Filters.regex('^Something else...$'), custom_choice) ],
-           TYPING_CHOICE: [
-               MessageHandler(Filters.text & ~(Filters.command | Filters.regex('^Done$')), regular_choice) ],
-           TYPING_REPLY: [
-               MessageHandler(Filters.text & ~(Filters.command | Filters.regex('^Done$')), received_information) ],
-           },
-        fallbacks=MessageHandler(Filters.regex('^Done$'), done)
+            TYPING_MESSAGE: [
+                MessageHandler(
+                    Filters.text & ~(Filters.command | Filters.regex('^Exit$')),
+                    fwd_msg,
+                )
+            ],
+            TYPING_DESTINATION: [
+                MessageHandler(
+                    Filters.text & ~(Filters.command | Filters.regex('^Exit$')),
+                    destination,
+                )
+            ],
+            GO: [
+                MessageHandler(
+                    Filters.text & ~(Filters.command | Filters.regex('^Exit$')),
+                    send,
+                )
+            ],
+        },
+        fallbacks=[MessageHandler(Filters.regex('^Exit$'), exit)],
+    )
 
     dp.add_handler(conv_handler)
-
+    
     # log all errors
     dp.add_error_handler(error)
 
     # Start the Bot
-    updater.start_webhook(listen="0.0.0.0",
-                          port=int(PORT),
-                          url_path=TOKEN)
-    """replace link with the link to your app"""
-    updater.bot.setWebhook('link' + TOKEN)
+    updater.start_polling()
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_webhook is non-blocking and will stop the bot gracefully.
+    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
 if __name__ == '__main__':
